06ede7c3a74a235e472ad43020a822b38e44fbbe
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2025-05-07 14:57:36 +0900

    Formulaファイルを作成.

---
+++ Formula/pbvr.rb
@@ -0,0 +1,45 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PBVR < Formula
+  desc ""
+  homepage "https://github.com/CCSEPBVR/CS-IS-PBVR"
+  url "https://github.com/CCSEPBVR/CS-IS-PBVR/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "98d053a524a824cc5e14ccd1ef949e7faa4bbbab5ab98c3550ee4b32921f5ec2"
+  license ""
+
+  # depends_on "cmake" => :build
+  depends_on "make" => :build
+  depends_on "gcc" => :build
+  depends_on "libomp"
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    ENV.append "CXXFLAGS", "-Xpreprocessor -fopenmp -I#{Formula["libomp"].opt_include}"
+    ENV.append "LDFLAGS", "-L#{Formula["libomp"].opt_lib} -lomp"
+    system "make", "-C", "CS_server"
+    bin.install "CS_server/pbvr_server"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test CS-IS-PBVR`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
