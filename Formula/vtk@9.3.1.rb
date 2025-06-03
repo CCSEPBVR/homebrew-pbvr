90f21f51e8fcc5212d37e63a3d65e95729df4b3a
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2025-06-03 11:26:43 +0900

    VTKのFormulaファイルを修正.

--- Formula/vtk@9.3.1.rb
+++ Formula/vtk@9.3.1.rb
@@ -7,12 +7,11 @@ class VtkAT931 < Formula
 
   depends_on "cmake" => [:build]
 
+  patch :DATA
+
   def install
     mkdir "build" do
       # cmakeの引数の設定
-      ENV["CC"]="/usr/bin/cc"
-      ENV["CXX"]="/usr/bin/c++"
-
       args = %W[
         -DCMAKE_INSTALL_PREFIX=#{prefix}
         -DCMAKE_BUILD_TYPE=Release
@@ -42,3 +41,28 @@ class VtkAT931 < Formula
     system "false"
   end
 end
+
+
+__END__
+diff --git a/ThirdParty/diy2/vtkdiy2/include/vtkdiy2/fmt/format.h b/ThirdParty/diy2/vtkdiy2/include/vtkdiy2/fmt/format.h
+index dcf4a399..dbce107d 100644
+--- a/ThirdParty/diy2/vtkdiy2/include/vtkdiy2/fmt/format.h
++++ b/ThirdParty/diy2/vtkdiy2/include/vtkdiy2/fmt/format.h
+@@ -480,6 +480,7 @@ void buffer<T>::append(const U* begin, const U* end) {
+ }
+ }  // namespace internal
+ 
++#ifdef __cpp_char8_t
+ // A UTF-8 string view.
+ class u8string_view : public basic_string_view<char8_t> {
+  public:
+@@ -496,7 +497,8 @@ inline u8string_view operator"" _u(const char* s, std::size_t n) {
+   return {s, n};
+ }
+ }  // namespace literals
+-#endif
++#endif // FMT_USE_USER_DEFINED_LITERALS
++#endif // __cpp_char8_t
+ 
+ // The number of characters to store in the basic_memory_buffer object itself
+ // to avoid dynamic memory allocation.
